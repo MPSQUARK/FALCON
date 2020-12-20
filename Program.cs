d326fdcf61088aef33b66e7a4892639e180a945d
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2020-12-20 22:40:05 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MachineLearningSpectralFittingCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
