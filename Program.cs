3a2477505a8d4233ba79948dd138ea23b3d799a9
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2020-12-31 20:13:45 +0000

    Added GPU-based Vector functionality

--- Program.cs
+++ Program.cs
@@ -1,32 +1,61 @@
-﻿using System;
+﻿using ILGPU;
+using ILGPU.Runtime;
+using System;
+using System.Collections.Generic;
 
 
 namespace MachineLearningSpectralFittingCode
 {
     class Program : ProcessDataMethods
     {
-        
-
 
         static void Main()
         {
-            string Data_path = @"C:\Users\Marcelpaw\source\repos\MachineLearningSpectralFittingCode\spec-0266-51602-0001.dat";
+            //string Data_path = @"C:\Users\Marcelpaw\source\repos\MachineLearningSpectralFittingCode\spec-0266-51602-0001.dat";
 
-            ReadData(Data_path);
+            //ReadData(Data_path);
+            Vector vectorA = new Vector(new float[3] { 1, 3, -5 });
+            Vector vectorB = new Vector(new float[3] { 4, -2, -1 });
+            float scalar = 5f;
+
+            List<AcceleratorId> AcceleratorIds = new List<AcceleratorId>();
+
+            foreach (var accelerator in Accelerator.Accelerators)
+            {
+                AcceleratorIds.Add(accelerator);
+            }
+
+            //Vector Output = Vector.ScalarProduct(AcceleratorIds[1] ,vectorA, scalar);
 
 
+            //for (int i = 0; i < Output.value.Length; i++)
+            //{
+            //    Console.WriteLine(Output.value[i]);
+            //}
+
+            float test_val =  Vector.DotProduct(AcceleratorIds[1], vectorA, vectorB);
+            Console.WriteLine(test_val);
             
-            Console.WriteLine("Start");
+            
+
+            Console.WriteLine("End");
+
+
+
+
+
+
+            //Console.WriteLine("Start");
 
 
             // Read Data in 
             // Read Modles is 
-            
+
 
             // MW & Background Reddening
             // Dust attenuation
 
-            
+
             // UI
 
             // LEARNING
@@ -36,7 +65,7 @@ namespace MachineLearningSpectralFittingCode
             // OUTPUT
 
             // RUN PYTHON FOR VISUALS
-            
+
 
         }
     }
---
+++ Vector.cs
@@ -0,0 +1,124 @@
+﻿using ILGPU;
+using ILGPU.Runtime;
+using System;
+using System.Linq;
+
+namespace MachineLearningSpectralFittingCode
+{
+    public class Vector
+    {
+        public Vector(float[] value)
+        {
+            this.value = value;
+        }
+
+        public float[] value { get; set; }
+
+        public static Vector ScalarProduct(AcceleratorId acceleratorId, Vector vector, float scalar)
+        {
+            using(var context = new Context())
+            {
+                using (var accelerator = Accelerator.Create(context, acceleratorId))
+                {
+                    var kernel = accelerator.LoadAutoGroupedStreamKernel<Index1, ArrayView<float>, ArrayView<float>, float>(ScalarProductKernal);
+
+                    var buffer  = accelerator.Allocate<float>(vector.value.Length);
+                    var buffer2 = accelerator.Allocate<float>(vector.value.Length);
+                    buffer.MemSetToZero();
+                    buffer2.MemSetToZero();
+
+                    // NEED TO ASSIGN vector.value to buffer2
+                    buffer2.CopyFrom(vector.value, 0, 0, vector.value.Length);
+
+                    kernel(buffer.Length, buffer.View, buffer2.View , scalar);
+
+                    accelerator.Synchronize();
+
+                    float[] Output = buffer.GetAsArray();
+
+                    buffer.Dispose();
+                    buffer2.Dispose();
+
+                    return new Vector(Output);
+                }
+            }
+        }
+
+        static void ScalarProductKernal(Index1 index, ArrayView<float> OutPut, ArrayView<float> Input, float Scalar )
+        {
+
+            OutPut[index] = Input[index] * Scalar;
+
+        }
+
+        public static Vector ConsecutiveProduct(AcceleratorId acceleratorId, Vector vectorA, Vector vectorB)
+        {
+            if (vectorA.value.Length != vectorB.value.Length)
+            {
+                throw new IndexOutOfRangeException("Vector A and Vector B provided MUST be of EQUAL length" );
+            }
+
+
+            using (var context = new Context())
+            {
+                using (var accelerator = Accelerator.Create(context, acceleratorId))
+                {
+                    var kernel = accelerator.LoadAutoGroupedStreamKernel<Index1, ArrayView<float>, ArrayView<float>, ArrayView<float>>(ConsecutiveProductKernal);
+
+                    var buffer = accelerator.Allocate<float>(vectorA.value.Length); // Input
+                    var buffer2 = accelerator.Allocate<float>(vectorA.value.Length); // Input
+                    var buffer3 = accelerator.Allocate<float>(vectorA.value.Length); // Output
+                    buffer.MemSetToZero();
+                    buffer2.MemSetToZero();
+                    buffer3.MemSetToZero();
+
+                    // NEED TO ASSIGN vector.value to buffer2
+                    buffer.CopyFrom(vectorA.value, 0, 0, vectorA.value.Length);
+                    buffer2.CopyFrom(vectorB.value, 0, 0, vectorB.value.Length);
+
+                    kernel(buffer.Length, buffer.View, buffer2.View, buffer3.View);
+
+                    accelerator.Synchronize();
+
+                    float[] Output = buffer3.GetAsArray();
+
+                    buffer.Dispose();
+                    buffer2.Dispose();
+                    buffer3.Dispose();
+
+                    return new Vector(Output);
+                }
+            }
+        }
+
+        static void ConsecutiveProductKernal(Index1 index, ArrayView<float> InputA, ArrayView<float> InputB, ArrayView<float> OutPut )
+        {
+
+            OutPut[index] = InputA[index] * InputB[index];
+
+        }
+
+        public static float DotProduct(AcceleratorId acceleratorId, Vector vectorA, object param )
+        {
+            if (param.GetType() == typeof(Vector))
+            {
+                return ConsecutiveProduct(acceleratorId, vectorA, (Vector)param).value.Sum();
+            }
+            else if (param.GetType() == typeof(float))
+            {
+                return ScalarProduct(acceleratorId, vectorA, (float)param).value.Sum();
+            }
+            else 
+            {
+                throw new Exception("Wrong input param object, accepts only T<Vector> and T<float>");
+            }
+        }
+
+
+
+    }
+
+
+
+
+}
